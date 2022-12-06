e9655f4569edd85398859dcafe73f337d4aad359
S Sanjith <83284162+S-Sanjith@users.noreply.github.com>
2022-12-06 10:48:00 +0530

    Add files via upload

--- 8_puzzle_ids.py
+++ 8_puzzle_ids.py
@@ -17,8 +17,36 @@ def id_dfs(puzzle, goal, get_moves):
         if route:
             return route
 
+def num_moves(rows, cols):
+    def get_moves(subject):
+        moves = []
+
+        zrow, zcol = next((r, c)
+            for r, l in enumerate(subject)
+                for c, v in enumerate(l) if v == 0)
+
+        def swap(row, col):
+            import copy
+            s = copy.deepcopy(subject)
+            s[zrow][zcol], s[row][col] = s[row][col], s[zrow][zcol]
+            return s
+
+        # north
+        if zrow > 0:
+            moves.append(swap(zrow - 1, zcol))
+        # east
+        if zcol < cols - 1:
+            moves.append(swap(zrow, zcol + 1))
+        # south
+        if zrow < rows - 1:
+            moves.append(swap(zrow + 1, zcol))
+        # west
+        if zcol > 0:
+            moves.append(swap(zrow, zcol - 1))
+
+        return moves
+    return get_moves
 
 puzzle, goal = [[1,2,5],[3,4,0],[6,7,8]], [[0,1,2],[3,4,5],[6,7,8]] # num_matrix(3, 3)
 solution = id_dfs(puzzle, goal, num_moves(3, 3))
 print(solution)
-
